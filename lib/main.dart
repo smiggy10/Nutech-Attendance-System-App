15297130c175ff88ad9a155625dcb320f538fa09
Miggy Raphael Bayhon <miggybayhon@gmail.com>
2026-03-06 13:23:33 +0800

    Add device preview for testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:nutech_app/app.dart';
 
 void main() {
-  runApp(const NutechApp());
+  runApp(DevicePreview(enabled: true, builder: (context) => const NutechApp()));
 }
