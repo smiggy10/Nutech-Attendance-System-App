ce17694f484f340dba3a4844d4eef59e71b90da3
Miggy Raphael Bayhon <miggybayhon@gmail.com>
2026-03-06 10:42:23 +0800

    backend

--- nutech_app/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:nutech_app/app.dart';
-
-void main() {
-  runApp(const NutechApp());
-}
--- nutech_app/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:nutech_app/app.dart';
-
-void main() {
-  testWidgets('App builds', (WidgetTester tester) async {
-    await tester.pumpWidget(const NutechApp());
-    expect(find.byType(NutechApp), findsOneWidget);
-  });
-}
