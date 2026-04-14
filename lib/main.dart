0ce3b165ed01c35068979e3d54d05ec903f0760c
knudcengradly <119305189+knudcengradly@users.noreply.github.com>
2026-04-14 09:08:29 +0800

    device preview removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:nutech_app/app.dart';
 
 void main() {
-  runApp(DevicePreview(enabled: true, builder: (context) => const NutechApp()));
+  runApp(const NutechApp());
 }
